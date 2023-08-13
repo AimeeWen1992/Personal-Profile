6a65111bb2d9337ad81940d4708b5e46ef10ad5f
Aimee <rwen1@myseneca.ca>
2023-08-12 22:23:36 -0400

    update section 1 to 4

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const hiringRadio = document.querySelector('input[value="Hiring"]');
 const hourlyRateLabel = document.getElementById("hourlyRateLabel");
 const hourlyRateContainer = document.getElementById("hourlyRateContainer");
-
 const questionCommentLabel = document.getElementById("question/commentLabel");
 const questionCommentContainer = document.getElementById(
   "question/commentContainer"
@@ -10,23 +9,35 @@ const questionCommentContainer = document.getElementById(
 const radios = document.querySelectorAll('input[name="contact-type"]');
 radios.forEach((radio) => {
   radio.addEventListener("change", function () {
-    if (this.value === "Hiring") {
-      hourlyRateLabel.style.display = "inline";
-      hourlyRateContainer.style.display = "inline-block";
-      questionCommentLabel.style.display = "none";
-      questionCommentContainer.style.display = "none";
-    }
     if (this.value === "Question") {
       questionCommentLabel.style.display = "inline";
       questionCommentContainer.style.display = "inline-block";
       hourlyRateLabel.style.display = "none";
       hourlyRateContainer.style.display = "none";
+      warningMessage.style.display = "none";
     }
     if (this.value === "Comment") {
       questionCommentLabel.style.display = "inline";
       questionCommentContainer.style.display = "inline-block";
       hourlyRateLabel.style.display = "none";
       hourlyRateContainer.style.display = "none";
+      warningMessage.style.display = "none";
+    }
+    if (this.value === "Hiring") {
+      hourlyRateLabel.style.display = "inline";
+      hourlyRateContainer.style.display = "inline-block";
+      questionCommentLabel.style.display = "none";
+      questionCommentContainer.style.display = "none";
     }
   });
 });
+
+function checkHourlyRate() {
+  var hourlyRateInput = document.getElementById("hourlyRateContainer");
+  var value = parseFloat(hourlyRateInput.value);
+
+  if (value < 16) {
+    alert("Illegal!!! Hourly rate should be at least $16/hr!!!!!!");
+    hourlyRateInput.value = "";
+  }
+}
